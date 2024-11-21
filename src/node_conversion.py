87df8ce5465bc91703d0d94efa4414efd13b1e7f
pouyannc <pouyannc@gmail.com>
2024-11-21 18:15:55 -0500

    split delimiter function to break down normal text nodes

---
+++ src/node_conversion.py
@@ -0,0 +1,38 @@
+from htmlnode import LeafNode
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        old_type = old_node.text_type
+        if old_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        i = 0
+        node_arr = old_node.text.split(delimiter)
+        if len(node_arr) % 2 == 0:
+            raise Exception("Invalid markdown syntax")
+        for node_text in node_arr:
+            if i % 2 == 0:
+                new_nodes.append(TextNode(node_text, old_type))
+            else:
+                new_nodes.append(TextNode(node_text, text_type))
+            i += 1
+    return new_nodes
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type.value:
+        case "text":
+            return LeafNode(None, text_node.text)
+        case "bold":
+            return LeafNode('b', text_node.text)
+        case "italic":
+            return LeafNode('i', text_node.text)
+        case "code":
+            return LeafNode('code', text_node.text)
+        case "link":
+            return LeafNode('a', text_node.text, {"href": text_node.url})
+        case "image":
+            return LeafNode('img', '', {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("Not a valid text type")
--- src/test_node_conversion.py
+++ src/test_node_conversion.py
@@ -2,7 +2,7 @@ import unittest
 
 from textnode import TextNode, TextType
 from htmlnode import LeafNode
-from main import text_node_to_html_node
+from node_conversion import text_node_to_html_node, split_nodes_delimiter
 
 class TestTextToHTML(unittest.TestCase):
     def test_convert_normal_text(self):
@@ -16,3 +16,41 @@ class TestTextToHTML(unittest.TestCase):
     def test_convert_link(self):
         textnode = TextNode('some link', TextType.LINK, "google.com")
         self.assertEqual(text_node_to_html_node(textnode), LeafNode('a', 'some link', {"href": "google.com"}))
+
+class TestSplitDelimiter(unittest.TestCase):
+    def test_split_bold(self):
+        nodes = [TextNode("this is **bold** text", TextType.TEXT), TextNode("**and even** more **bold** text", TextType.TEXT)]
+        self.assertEqual(
+            split_nodes_delimiter(nodes, '**', TextType.BOLD),
+            [
+                TextNode("this is ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" text", TextType.TEXT),
+                TextNode("", TextType.TEXT),
+                TextNode("and even", TextType.BOLD),
+                TextNode(" more ", TextType.TEXT),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" text", TextType.TEXT)
+            ]
+            )
+    
+    def test_split_code(self):
+        nodes = [TextNode("this is `code` text", TextType.TEXT), TextNode("and `code` and `more code` text", TextType.TEXT)]
+        self.assertEqual(
+            split_nodes_delimiter(nodes, '`', TextType.CODE),
+            [
+                TextNode("this is ", TextType.TEXT),
+                TextNode("code", TextType.CODE),
+                TextNode(" text", TextType.TEXT),
+                TextNode("and ", TextType.TEXT),
+                TextNode("code", TextType.CODE),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("more code", TextType.CODE),
+                TextNode(" text", TextType.TEXT)
+            ]
+            )
+    
+    def test_invalid_md(self):
+        nodes = [TextNode("something **invalid", TextType.TEXT)]
+        with self.assertRaises(Exception):
+            split_nodes_delimiter(nodes, "**", TextType.BOLD)
