6e5e07e24fee9b48846ec741d0bcb6f0151a7a87
pouyannc <pouyannc@gmail.com>
2024-11-28 22:54:01 -0500

    Static directory and script to build a public directory and copy static files over

--- src/main.py
+++ src/main.py
@@ -1,15 +1,37 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode
+import os
+import shutil
 
 def main():
-    new_textnode = TextNode("this is a text node", TextType.BOLD, 'https://www.boot.dev')
-    print(new_textnode)
+    build()
+    return
 
-    tag = "a"
-    value = "Some text"
-    props = {"href": "google.com"}
-    html_node = HTMLNode(tag, value, props=props)
-    print(html_node)
+def build():
+    print("Current working directory:", os.getcwd())
+    if os.path.exists("./public"):
+        print("Removing current public directory...")
+        shutil.rmtree("./public")
+    os.mkdir("./public")  
+    print("Copying contents to public: ", os.listdir("./static"))
+    copy_files_r("./static", "./public")
+    return
+
+def copy_files_r(source, dest):
+    if os.path.isfile(source):
+        print("Cannot copy contents of a file (path to file given)")
+        return
+
+    contents = os.listdir(source)
+    for item in contents:
+        source_path = os.path.join(source, item)
+        if os.path.isfile(source_path):
+            print("file copied:", shutil.copy(source_path, dest))
+        else:
+            dest_path = os.path.join(dest, item)
+            os.mkdir(dest_path)
+            copy_files_r(source_path, dest_path)
+    return
 
 if __name__ == "__main__":
     main()
